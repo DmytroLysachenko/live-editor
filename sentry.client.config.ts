928476b1cc138aa3cf46e8b89fd70c14859b63dc
DmytroLysachenko <dlysachenko98@gmail.com>
2025-06-24 19:53:43 +0200

    Removed whitespaces

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,14 +2,9 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://ff37ea59ee51fe40dcc2d5c8e840ab41@o4508646704349184.ingest.de.sentry.io/4508808460501072",
-
   integrations: [Sentry.replayIntegration()],
-
   tracesSampleRate: 1,
-
   replaysSessionSampleRate: 0.1,
-
   replaysOnErrorSampleRate: 1.0,
-
   debug: false,
 });
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,8 +2,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://ff37ea59ee51fe40dcc2d5c8e840ab41@o4508646704349184.ingest.de.sentry.io/4508808460501072",
-
   tracesSampleRate: 1,
-
   debug: false,
 });
